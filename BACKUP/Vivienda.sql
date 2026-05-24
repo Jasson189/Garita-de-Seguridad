cc79aaf5452078a6d2dc7f8580ba041577dc3a18
Jelson Jose sicay Lopez <jelsonxlntres@gmail.com>
2026-05-23 22:44:26 -0600

    Delete BACKUP directory

--- BACKUP/Vivienda.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE viviendas (
-    id_vivienda SERIAL PRIMARY KEY,
-    numero_vivienda VARCHAR(20) NOT NULL,
-    sector VARCHAR(50),
-    direccion_referencia VARCHAR(150),
-    estado BOOLEAN NOT NULL DEFAULT TRUE,
-    fecha_registro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
--- BACKUP/nombre_rol.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO roles (nombre_rol, descripcion)
-VALUES 
-('Administrador', 'Control total del sistemas'),
-('Agente', 'Encargado de registrar visitas'),
-('Vecino', 'Residente de la vivienda');
